06bb005666bd78ea3da8f740d15be92f9222f8ca
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 15:11:18 -0500

    /*merge conflicts*/

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -5,7 +5,6 @@ import Results from './Results.jsx';
 import Home from './HomePage.jsx';
 import Profile from './Profile.jsx';
 
-
 export default class HomeContainer extends Component {
   state = {
     vendors: null
