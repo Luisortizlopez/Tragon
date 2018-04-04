abc7ab39a11234b41d91ef1a4dcaeebfce0ed587
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:28:56 -0500

    /*Merge Conflicts*/

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,45 +1,7 @@
 import React from 'react';
-<<<<<<< dd70aa246853f8d0097c2d137aa326e1f504a898
 import ProfilePage from '../components/home/ProfileComp.jsx';
 
-=======
-<<<<<<< ee2d5a2340e26bd2ac68475b6558f6694b56dedc
-=======
-<<<<<<< 611051f6ecbeb8332bde510fb085749bbf28edc8
-<<<<<<< e619b692fceb5b46c242dd4ef7959bc00957f8a7
->>>>>>> /*Fixing mergin problems*/
-<<<<<<< 1d3a09abbcbbb7149757c4b62079173ab38ad02c
-<<<<<<< 84b6d1eadaa4520ebe85a517e1282c75b9773843
-import ProfilePage from '../components/home/ProfileComp.jsx';
 
-<<<<<<< ca997bc8a49fa8b05abc68697369324c882822e6
-<<<<<<< 78b5499c5ef91a5ffe368165e798111d339ed8e0
-export default class Profile extends Component {
-=======
-=======
-=======
-=======
->>>>>>> /*merge problems*/
-import MyGoogleMap from '../components/Map/MapConfig.jsx';
-import CardExampleWithAvatar from '../components/profile/AvatarVender.jsx';
->>>>>>> /*Fixing mergin problems*/
-<<<<<<< ee2d5a2340e26bd2ac68475b6558f6694b56dedc
-=======
-=======
-import ProfilePage from '../components/home/ProfileComp.jsx';
->>>>>>> trying to render avatar in profile page
-=======
-<<<<<<< de44d9351c0e984eee47cfd11d52479c46a0a393
-import ProfilePage from '../components/home/ProfileComp.jsx';
-
-=======
-import MyGoogleMap from '../components/Map/MapConfig.jsx';
-import CardExampleWithAvatar from '../components/profile/AvatarVender.jsx';
->>>>>>> /*Fixing mergin problems*/
->>>>>>> /*Fixing mergin problems*/
->>>>>>> /*Fixing mergin problems*/
-
->>>>>>> /*Fixing mergin problems*/
  class Profile extends React.Component {
 
   state = {
