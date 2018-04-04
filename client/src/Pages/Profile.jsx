78fb33bbd302394d4af9486d5407be4058e97728
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 14:54:06 -0500

    profile page in process

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -26,9 +26,13 @@ import ProfilePage from '../components/home/ProfileComp.jsx';
 
   render() {
     return (
+<<<<<<< d885ffb06506d98a21bb556d9c647f0f587c204f
       <div>
       <ProfilePage />
       </div>
+=======
+      <ProfilePage />
+>>>>>>> profile page in process
     )
   }
 }
