67145d4147054f3b3be55fc7a16055e6afb39548
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:01:09 -0500

    /*Fixing mergin problems*/

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+<<<<<<< 84b6d1eadaa4520ebe85a517e1282c75b9773843
 import ProfilePage from '../components/home/ProfileComp.jsx';
 
+<<<<<<< d2b73761102f658885e1a4725f8999ffd7ef3b80
 <<<<<<< 84cb8ec61b57a20f14756b83d6abe8f36c0f4f8e
 <<<<<<< 550edbf9e8e1ecd9dc98ce4215d112f333efa2ba
 export default class Profile extends Component {
 =======
 =======
+>>>>>>> /*Fixing mergin problems*/
+=======
 import MyGoogleMap from '../components/Map/MapConfig.jsx';
 import CardExampleWithAvatar from '../components/profile/AvatarVender.jsx';
 >>>>>>> /*Fixing mergin problems*/
+<<<<<<< d2b73761102f658885e1a4725f8999ffd7ef3b80
 =======
+=======
+>>>>>>> /*Fixing mergin problems*/
 
 <<<<<<< 9cdf7d0df792e7e0383c7e7c99b25e1c5707f923
 >>>>>>> /*Merge Conflicts*/
