ab47bc67f5de9e9f621e669afc3b41dd72350633
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:01:09 -0500

    /*Fixing mergin problems*/

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -3,10 +3,13 @@ import Select from 'react-select';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Results from './Results.jsx';
 import Home from './HomePage.jsx';
+<<<<<<< 84b6d1eadaa4520ebe85a517e1282c75b9773843
 <<<<<<< c238647732a37bab123421f420669429889eded9
 =======
 import ProfilePage from '../components/profileComp/ProfileComp.jsx';
 >>>>>>> /*merge conflicts*/
+=======
+>>>>>>> /*Fixing mergin problems*/
 import Profile from './Profile.jsx';
 
 
