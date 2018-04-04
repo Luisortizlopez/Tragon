16da0de5e9756dd97acc06de4f8a358285043c1e
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 12:26:04 -0500

    trying to render avatar in profile page

--- client/src/components/home/ProfileComp.jsx
+++ client/src/components/home/ProfileComp.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
+<<<<<<< 7dea444af3b1b1e974eb877e5acac3ce9cedad2f
 import UserName from './UserName.jsx';
 import Card from 'material-ui/Card';
+<<<<<<< eb393e807f04c39d86490f2f56c432b7d382f19e
 <<<<<<< abc7ab39a11234b41d91ef1a4dcaeebfce0ed587
 
 
 
 =======
 >>>>>>> added card background to profile info
+=======
+=======
+
+
+>>>>>>> trying to render avatar in profile page
+>>>>>>> trying to render avatar in profile page
 
 const style = {
     width: 160,
@@ -15,6 +23,7 @@ const style = {
 }
 
 
+<<<<<<< 7dea444af3b1b1e974eb877e5acac3ce9cedad2f
 
 const ProfilePage = (props) => (
   <div>
@@ -29,6 +38,13 @@ const ProfilePage = (props) => (
 >>>>>>> added card background to profile info
    </Card>
  </div>
+=======
+const ProfilePage = (props) => (
+  <div className='UserAvatar'>
+    <Avatar style={style} alt='UserAvatar' src='../css/TragonComiendo.jpg' className='Picture' />
+ </div>
+
+>>>>>>> trying to render avatar in profile page
 );
 
 
