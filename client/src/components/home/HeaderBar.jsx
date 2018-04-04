57e0c578d720207c0b9d088e54e9ff3c27683221
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:01:09 -0500

    /*Fixing mergin problems*/

--- client/src/components/home/HeaderBar.jsx
+++ client/src/components/home/HeaderBar.jsx
@@ -13,9 +13,7 @@ import Toolbar from 'material-ui/Toolbar';
             <Link to="/vendor"><Button id='AvatarVender' label="add-vendor" color="inherit">Add A New Vendor</Button></Link>
             <Button id='/Logout' label='log-out' color='inherit' onClick={this.props.logout}>Logout</Button>
             <Link to="login"><div className='Log'><Button id='Log-in' label="log-in" color="inherit">Login</Button></div></Link>
-            <div className='/Log'><Button id='Login' label="Userlogin" color="inherit">Log in</Button></div>
             <div className='Log'><Button id='Login' label="Userlogin" color="inherit">Log in</Button></div>
-
           </Toolbar>
       </AppBar>
     );
