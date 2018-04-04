ca997bc8a49fa8b05abc68697369324c882822e6
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 15:11:18 -0500

    /*merge conflicts*/

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -1,31 +1,8 @@
 import React, {Component} from 'react';
 import Select from 'react-select';
-<<<<<<< 919203272d76756a715454767cd153aa232841b2
-=======
-import {Switch, Route, withRouter} from 'react-router-dom';
-<<<<<<< efdde03a24c52adf3d03df2a25568871bc3ad237
-
->>>>>>> working in HomeContainer
-import Results from './Results.jsx';
-<<<<<<< 075605e6c57cd7ab37bfb74a5fb5b72994355bce
-import Home from './HomePage.jsx'
-=======
-import Home from './HomePage.jsx';
-<<<<<<< de44d9351c0e984eee47cfd11d52479c46a0a393
-<<<<<<< a2d1ac580904371a3dd6230f8e566711c0a02a71
-=======
-import ProfilePage from '../components/profileComp/ProfileComp.jsx';
->>>>>>> /*merge conflicts*/
-=======
->>>>>>> /*Fixing mergin problems*/
-import Profile from './Profile.jsx';
->>>>>>> /*Fixing mergin problems*/
-=======
 import Results from './Results.jsx';
 import Home from './HomePage.jsx';
-import ProfilePage from '../components/profileComp/ProfileComp.jsx';
 import Profile from './Profile.jsx';
->>>>>>> trying to render avatar in profile page
 
 export default class HomeContainer extends Component {
   state = {
@@ -56,18 +33,11 @@ export default class HomeContainer extends Component {
   render() {
     return (
       <div>
-<<<<<<< 919203272d76756a715454767cd153aa232841b2
         {this.state.vendors ? (
           <Results vendors={this.state.vendors} />
         ) : (
           <Home submitSearch={this.submitSearch} />
         )}
-=======
-        <Switch>
-          <Route exact path="/" render={(props) => <Home submitSearch={this.submitSearch} /> } />
-          <Route path='/profile' components={Profile} />
-        </Switch>
->>>>>>> working in HomeContainer
       </div>
     );
   }
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -30,6 +30,7 @@ export default class Profile extends Component {
   render() {
     const {picture, name} = this.state.person
     return (
+<<<<<<< 32c0952e5720f7dbf3d90626d54c5a98a0fc00cc
 <<<<<<< 7861a6a22543abea8b4ce779d578986b2319f233
 <<<<<<< 78b5499c5ef91a5ffe368165e798111d339ed8e0
       <div>
@@ -40,6 +41,10 @@ export default class Profile extends Component {
 =======
       <ProfilePage />
 >>>>>>> profile page in process
+=======
+<<<<<<< c238647732a37bab123421f420669429889eded9
+      <ProfilePage />
+>>>>>>> /*merge conflicts*/
 =======
       <div>
       <ProfilePage />
