66d65c381c82fc3cb9bd9676c9ac4f344cdad765
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 14:54:06 -0500

    profile page in process

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -7,6 +7,9 @@ import Profile from './Profile.jsx';
 
 
 
+import Profile from './Profile.jsx';
+
+>>>>>>> profile page in process
 export default class HomeContainer extends Component {
   state = {
     vendors: null
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ProfilePage from '../components/home/ProfileComp.jsx';
 
 
+<<<<<<< 8fa4dd0c5d24fc40af6312ba08ba281f8e5e05ae
 
+=======
+>>>>>>> profile page in process
  class Profile extends React.Component {
   state = {
     person: []
@@ -27,9 +30,7 @@ import ProfilePage from '../components/home/ProfileComp.jsx';
   render() {
     const {picture, name} = this.state.person
     return (
-      <div>
       <ProfilePage />
-      </div>
     )
   }
 }
