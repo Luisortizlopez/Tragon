7d51b5cd0f600980b1dc9ba3ce7f169fc2770074
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 14:54:06 -0500

    fixed merge conflict

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -26,6 +26,7 @@ import ProfilePage from '../components/home/ProfileComp.jsx';
   render() {
     const {picture, name} = this.state.person
     return (
+<<<<<<< 357737fb2932834b6ade47a9804a6ca65605ee62
 <<<<<<< d885ffb06506d98a21bb556d9c647f0f587c204f
       <div>
       <ProfilePage />
@@ -33,6 +34,9 @@ import ProfilePage from '../components/home/ProfileComp.jsx';
 =======
       <ProfilePage />
 >>>>>>> profile page in process
+=======
+      <ProfilePage />
+>>>>>>> fixed merge conflict
     )
   }
 }
