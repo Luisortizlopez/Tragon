489aa402d1cf44e4f9d2c6d8d5d177b09436ecf8
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 12:26:04 -0500

    trying to render avatar in profile page

--- client/src/components/home/ProfileComp.jsx
+++ client/src/components/home/ProfileComp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
+<<<<<<< 583ae7d11696aaeaa5276cf28af8f6287267cfb2
 <<<<<<< 9a075c964c58c78ef5d777f6e95d7ecc86ae0841
 <<<<<<< 7dea444af3b1b1e974eb877e5acac3ce9cedad2f
 import UserName from './UserName.jsx';
@@ -12,6 +13,10 @@ import Card from 'material-ui/Card';
 
 
 >>>>>>> /*merge conflicts*/
+=======
+
+
+>>>>>>> trying to render avatar in profile page
 
 const style = {
     width: 160,
@@ -19,6 +24,7 @@ const style = {
 }
 
 
+<<<<<<< 583ae7d11696aaeaa5276cf28af8f6287267cfb2
 <<<<<<< 9a075c964c58c78ef5d777f6e95d7ecc86ae0841
 <<<<<<< 7dea444af3b1b1e974eb877e5acac3ce9cedad2f
 
@@ -32,15 +38,20 @@ const ProfilePage = (props) => (
 =======
 =======
 >>>>>>> /*merge conflicts*/
+=======
+>>>>>>> trying to render avatar in profile page
 const ProfilePage = (props) => (
   <div className='UserAvatar'>
     <Avatar style={style} alt='UserAvatar' src='../css/TragonComiendo.jpg' className='Picture' />
  </div>
 
+<<<<<<< 583ae7d11696aaeaa5276cf28af8f6287267cfb2
 <<<<<<< 9a075c964c58c78ef5d777f6e95d7ecc86ae0841
 >>>>>>> trying to render avatar in profile page
 =======
 >>>>>>> /*merge conflicts*/
+=======
+>>>>>>> trying to render avatar in profile page
 );
 
 export default ProfilePage;
