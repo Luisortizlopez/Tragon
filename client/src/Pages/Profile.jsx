84cb8ec61b57a20f14756b83d6abe8f36c0f4f8e
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:01:09 -0500

    /*Fixing mergin problems*/

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+<<<<<<< de44d9351c0e984eee47cfd11d52479c46a0a393
 import ProfilePage from '../components/home/ProfileComp.jsx';
 
+<<<<<<< 550edbf9e8e1ecd9dc98ce4215d112f333efa2ba
 export default class Profile extends Component {
+=======
+=======
+import MyGoogleMap from '../components/Map/MapConfig.jsx';
+import CardExampleWithAvatar from '../components/profile/AvatarVender.jsx';
+>>>>>>> /*Fixing mergin problems*/
+
+ class Profile extends React.Component {
+>>>>>>> /*Fixing mergin problems*/
   state = {
     person: []
   }
