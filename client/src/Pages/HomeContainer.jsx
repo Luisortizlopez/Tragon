9855ff8309c0d82adf03b3d799d1f30b9373d7af
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 16:01:09 -0500

    /*Fixing mergin problems*/

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -5,20 +5,8 @@ import Results from './Results.jsx';
 import Home from './HomePage.jsx';
 import Profile from './Profile.jsx';
 
-<<<<<<< de011d176d58bd40aeaeb464e7fde63bbf2a90f7
-<<<<<<< 98c539d8ce26ee7da541a5c028b2551e687a1628
 
 
-
-
-
-=======
-import Profile from './Profile.jsx';
-
->>>>>>> profile page in process
->>>>>>> profile page in process
-=======
->>>>>>> /*merge conflicts*/
 export default class HomeContainer extends Component {
   state = {
     vendors: null
