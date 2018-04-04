9b8e5e9f9cdbc3321d367d09b4b94b26625c0a80
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 15:11:18 -0500

    /*merge conflicts*/

--- client/src/components/home/HeaderBar.jsx
+++ client/src/components/home/HeaderBar.jsx
@@ -5,12 +5,14 @@ import Button from 'material-ui/Button';
 import Toolbar from 'material-ui/Toolbar';
 
 
-  const HeaderBar = () => (
+
+  const HeaderBar = (props) => (
       <AppBar id='appBar'>
           <Toolbar>
             <Link to="/"><Button id='tabButtonHome' color="inherit">Home</Button></Link>
-            <Link to="/user/:id"><Button id='AvatarVender' label="profile" color="inherit">Profile</Button></Link>
-            <Link to="/vendor"><Button id='AvatarVender' label="profile" color="inherit">AddVendor</Button></Link>
+            <Link to="/search"><Button id='AvatarVender' label="results" color="inherit">Results</Button></Link>
+            <Link to="/add"><Button id='AddVendor' color="inherit">Add Vendor</Button></Link>
+            <Link to="/profile"><Button id='profile' color="inherit">Profile</Button></Link>
             <div className='Log'><Button id='Login' label="Userlogin" color="inherit">Log in</Button></div>
           </Toolbar>
       </AppBar>
