97acaf3e9328b8cdae24689bd2eb65da9aeb7262
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-04 15:11:18 -0500

    /*merge conflicts*/

--- client/src/Pages/HomeContainer.jsx
+++ client/src/Pages/HomeContainer.jsx
@@ -2,19 +2,9 @@ import React, {Component} from 'react';
 import Select from 'react-select';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Results from './Results.jsx';
-<<<<<<< 075605e6c57cd7ab37bfb74a5fb5b72994355bce
-import Home from './HomePage.jsx'
-=======
 import Home from './HomePage.jsx';
-<<<<<<< de44d9351c0e984eee47cfd11d52479c46a0a393
-<<<<<<< a2d1ac580904371a3dd6230f8e566711c0a02a71
-=======
-import ProfilePage from '../components/profileComp/ProfileComp.jsx';
->>>>>>> /*merge conflicts*/
-=======
->>>>>>> /*Fixing mergin problems*/
 import Profile from './Profile.jsx';
->>>>>>> /*Fixing mergin problems*/
+
 
 
 
