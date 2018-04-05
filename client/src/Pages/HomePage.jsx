343532a92918a8b11b32620fa6385491e270920a
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-05 11:08:25 -0500

    added card background to profile info

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -3,14 +3,18 @@ import Button from 'material-ui/Button';
 import SearchBar from '../components/home/SearchBar.jsx';
 import LocationMenu from '../components/home/LocationMenu.jsx';
 import ProfilePage from '../components/home/ProfileComp.jsx';
+<<<<<<< 6ea53490d68c8c6305ef29d712c229e63905d9b4
 
 
+=======
+>>>>>>> added card background to profile info
 
 
 const Home = (props) => (
   <div>
     <SearchBar submitSearch={props.submitSearch} />
     <LocationMenu />
+    <ProfilePage />
 
 <<<<<<< b75dc5e9b802b3ff568b161eb3cdde03f8f0396a
 
--- client/src/components/home/ProfileComp.jsx
+++ client/src/components/home/ProfileComp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
+<<<<<<< 6ea53490d68c8c6305ef29d712c229e63905d9b4
 <<<<<<< b75dc5e9b802b3ff568b161eb3cdde03f8f0396a
 import UserName from './UserName.jsx';
 import Card from 'material-ui/Card';
@@ -8,6 +9,10 @@ import RatingStars from './RatingComp.jsx'
 
 >>>>>>> /*merge conflicts*/
 
+=======
+import UserName from './UserName.jsx';
+import Card from 'material-ui/Card';
+>>>>>>> added card background to profile info
 
 const style = {
     width: 160,
@@ -15,12 +20,17 @@ const style = {
 }
 
 
+
 const ProfilePage = (props) => (
+<<<<<<< 6ea53490d68c8c6305ef29d712c229e63905d9b4
 <<<<<<< b75dc5e9b802b3ff568b161eb3cdde03f8f0396a
+=======
+>>>>>>> added card background to profile info
   <div>
     <Card className='ProfileCard' style={{backgroundColor: "#94b8b8"}}>
     <Avatar id='UserAvatar' style={style} alt='Avatar' src='../css/TragonComiendo.jpg' className='Picture' />
     <UserName id='userName' />
+<<<<<<< 6ea53490d68c8c6305ef29d712c229e63905d9b4
     <RatingStars />
    </Card>
  </div> )
@@ -28,8 +38,10 @@ const ProfilePage = (props) => (
 =======
   <div className='UserAvatar'>
     <Avatar style={style} alt='UserAvatar' src='../css/TragonComiendo.jpg' className='Picture' />
+=======
+   </Card>
+>>>>>>> added card background to profile info
  </div>
-
 );
 >>>>>>> /*merge conflicts*/
 
