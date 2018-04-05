eca20d7f51b2a69892b8026d3e817098b822521b
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-05 11:08:25 -0500

    added card background to profile info

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,14 +2,19 @@ import React, {Component} from 'react';
 import Button from 'material-ui/Button';
 import SearchBar from '../components/home/SearchBar.jsx';
 import LocationMenu from '../components/home/LocationMenu.jsx';
+<<<<<<< abc7ab39a11234b41d91ef1a4dcaeebfce0ed587
 
 
+=======
+import ProfilePage from '../components/home/ProfileComp.jsx';
+>>>>>>> added card background to profile info
 
 
 const Home = (props) => (
   <div>
     <SearchBar submitSearch={props.submitSearch} />
     <LocationMenu />
+    <ProfilePage />
 
   </div>
 )
--- client/src/components/home/ProfileComp.jsx
+++ client/src/components/home/ProfileComp.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import UserName from './UserName.jsx';
 import Card from 'material-ui/Card';
+<<<<<<< abc7ab39a11234b41d91ef1a4dcaeebfce0ed587
 
 
 
+=======
+>>>>>>> added card background to profile info
 
 const style = {
     width: 160,
@@ -15,9 +18,15 @@ const style = {
 
 const ProfilePage = (props) => (
   <div>
+<<<<<<< abc7ab39a11234b41d91ef1a4dcaeebfce0ed587
     <Card className='ProfileCard' style={{backgroundColor: "#E6E6FA"}}>
     <Avatar id='UserAvatar' style={style} alt='Avatar' src='../css/TragonComiendo.jpg' className='Picture' />
     <UserName id='UsersName' />
+=======
+    <Card className='ProfileCard' style={{backgroundColor: "#94b8b8"}}>
+    <Avatar id='UserAvatar' style={style} alt='Avatar' src='../css/TragonComiendo.jpg' className='Picture' />
+    <UserName id='userName' />
+>>>>>>> added card background to profile info
    </Card>
  </div>
 );
