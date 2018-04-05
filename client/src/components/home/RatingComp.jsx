b1f67d15a32ac048d9617a757eb4367a08cbca4f
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-04-05 17:03:28 -0500

    installed react-star-rating-component npm pack for rating component

--- client/src/components/home/RatingComp.jsx
+++ client/src/components/home/RatingComp.jsx
@@ -1,13 +1,33 @@
-import { Rating } from 'material-ui-rating';
+import React from 'react';
+import StarRatingComponent from 'react-star-rating-component';
 
-const RatingStars = () => (
-  <div>
-       <Rating
-         value={3}
-         max={5}
-         onChange={(value) => console.log(`Rated with value ${value}`)}
-       />
-   </div>
-)
 
-export default RatingStars;
+export default class RatingStars extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      rating: 0
+    };
+  }
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
+  }
+
+  render() {
+    const { rating } = this.state;
+
+    return (
+      <div>
+        <h2>Rating:</h2>
+        <StarRatingComponent
+          className="rate1"
+          starCount={5}
+          value={rating}
+          onStarClick={this.onStarClick.bind(this)}
+        />
+      </div>
+    );
+  }
+}
